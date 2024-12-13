2c0e3cbeb36cf9ba162cd5535e016b92d846de54
tucaz <me@tucaz.net>
2024-12-13 04:14:07 +0000

    Adding swagger documentation

--- Gemfile
+++ Gemfile
@@ -1,11 +1,12 @@
 source 'https://rubygems.org'
 
 gem 'sinatra'
-gem 'sinatra-cors'  # To allow browser requests
-gem 'nokogiri'      # If not already included
+gem 'sinatra-cors'  
+gem 'sinatra-contrib'
+gem 'nokogiri'      
 gem 'rackup'
 gem 'puma'
 
 group :development, :test do
-  gem 'rspec', '~> 3.12.0'   # For testing
+  gem 'rspec', '~> 3.12.0'
 end 
--- app.rb
+++ app.rb
@@ -1,32 +1,92 @@
 require 'sinatra'
+require 'sinatra/json'
 require 'sinatra/cors'
-require 'json'
 require_relative 'lib/html_parser'
 
-set :bind, '0.0.0.0'  # Allow external connections
-set :port, 4567       # Default Sinatra port
+class App < Sinatra::Base
+  helpers Sinatra::JSON
 
-# Enable CORS for all routes
-set :allow_origin, "*"
-set :allow_methods, "GET,HEAD,POST"
-set :allow_headers, "content-type,if-modified-since"
+  # Enable CORS
+  set :allow_origin, "*"
+  set :allow_methods, "GET,HEAD,POST"
+  set :allow_headers, "content-type,if-modified-since"
+  set :bind, '0.0.0.0'
+  set :port, 4567
 
-# Create endpoint to parse HTML
-get '/search' do
-  content_type :json
+  # Serve Swagger UI
+  get '/api-docs' do
+    content_type :json
+    {
+      openapi: '3.0.0',
+      info: {
+        title: 'Google Search API',
+        version: '1.0.0',
+        description: 'API for parsing artwork information from Google search results'
+      },
+      paths: {
+        '/search': {
+          get: {
+            summary: 'Searches Google for Artwork',
+            parameters: [
+              {
+                name: 'scenario',
+                in: 'query',
+                schema: { type: 'string', default: 'default' },
+                description: 'Scenario to parse (valid values: default, van_gogh, leonardo_da_vinci)'
+              }
+            ],
+            responses: {
+              '200': {
+                description: 'Success',
+                content: {
+                  'application/json': {
+                    schema: {
+                      type: 'object',
+                      properties: {
+                        artworks: {
+                          type: 'array',
+                          items: {
+                            type: 'object',
+                            properties: {
+                              name: { type: 'string', description: 'Name of the artwork' },
+                              extensions: { 
+                                type: 'array', 
+                                items: { type: 'string' }, 
+                                description: 'Additional information like dates' 
+                              },
+                              link: { type: 'string', description: 'Link to Google search' },
+                              image: { type: 'string', description: 'Image data for thumbnail' }
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }.to_json
+  end
   
-  # Get scenario from query params, default to 'default'
-  scenario = params[:scenario] || 'default'
-  
-  # Build filename with .html extension
-  filename = "#{scenario}.html"
+  get '/search' do
+    content_type :json
+    scenario = params[:scenario] || 'default'
+    filename = "#{scenario}.html"
+    
+    html = File.read(File.join(__dir__, 'spec/fixtures', filename))
+    parser = HtmlParser.new(html)
+    result = parser.parse
+    
+    json result
+  end
 
-  puts "Filename: #{filename}"
-  
-  # Read the HTML file
-  html = File.read(File.join(__dir__, 'spec/fixtures', filename))
-  parser = HtmlParser.new(html)
-  result = parser.parse
-  
-  result.to_json
+  # Add a route to serve the Swagger UI
+  get '/' do
+    erb :swagger
+  end
+
+  run! if app_file == $0
 end
